6d769c207aa6dca4b9cd1364549162761b6517b2
njosuedev <njosueofficial@gmail.com>
2025-02-28 10:46:02 +0200

    server and client conn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 import express from "express";
 import http from "http";
 import { fileURLToPath } from "node:url";
-import { dirname,join } from "node:path";
-import {Server} from "socket.io";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import { Socket } from "node:dgram";
 
 // instance
 
@@ -15,9 +16,19 @@ const io = new Server(server);
 // serving html file
 
 const __direname = dirname(fileURLToPath(import.meta.url));
-app.get("/",(req,res) => res.send(join(__direname,"index.html")));
+app.get("/", (req, res) => res.sendFile(join(__direname, "index.html")));
+
+// defining a connection event handler
+
+io.on("connection", (client) => {
+    console.log("user connected to ( server )");
+
+   client.on("disconnect",() => {
+    console.log("user disconnected from server");
+   })
+})
 
 // Start the server
 
 const PORT = 3000;
-server.listen(PORT,() => console.log(`Server is running on : ${PORT}`));
+server.listen(PORT, () => console.log(`Server is running on : ${PORT}`));
