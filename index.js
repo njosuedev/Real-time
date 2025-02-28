588cf50c4e567bd4c22bd9cc86c11bc0d9d9f941
njosuedev <njosueofficial@gmail.com>
2025-02-28 08:25:50 +0200

    add packages

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello josue')
+// packages
+
+import express from "express";
+import http from "http";
+import { fileURLToPath } from "node:url";
+import { dirname,join } from "node:path";
+import {Server} from "socket.io";
+
+// instance
+
+console.log("server is running here!")
